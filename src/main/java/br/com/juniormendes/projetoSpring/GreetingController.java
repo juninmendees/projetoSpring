c08b89d230ecebfa4995a8bf888bab5cecddaa2b
juniormendes <juniormendesjp@gmail.com>
2023-11-26 15:11:51 -0300

    commit

--- src/main/java/br/com/juniormendes/projetoSpring/GreetingController.java
+++ src/main/java/br/com/juniormendes/projetoSpring/GreetingController.java
@@ -16,6 +16,7 @@ public class GreetingController {
            @RequestParam(value= "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(),String.format(template,name));
     }
+
 }
 
 
